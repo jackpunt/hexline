e755a1cd28f2ee5042d4687f6f0230d9d2d79581
jackpunt <jpeckj@gmail.com>
2022-04-22 07:33:05 -0700

    before trying yield/cc in yieldR

--- src/app/hex-intfs.ts
+++ src/app/hex-intfs.ts
@@ -1,50 +1,9 @@
 import { stime } from "@thegraid/createjs-lib"
 import { EventDispatcher } from "createjs-module"
+import moment = require("moment")
 
 /**
-We want to enable event processing during long-running, mutually recursive function calls.
-(for example, a recursive tree search)
-After a certain depth or time, the search wants to voluntarily suspend execution
-to allow the top level Event Loop to run (handle mouse/key events, repaint graphics, etc)
-
-The ideal would be a system-level function to runEventLoop()
-which 'yield' the current computation, put its own continuation on the event queue,
-and throw control to the system EventLoop.
-
-It seems that Javascript provides only partial solutions for this:
-'setTimeout()' will put a function on the event queue [but not the current continuation]
-'yield' will suspend the current continuation, but not put it on the event queue.
-But 'yield' returns a value to the Generator's caller one level up the call stack.
-And that caller must already have the 'continuation' in form of the Generator.
-
-We also note that although an uncaught 'throw' will return control to the top-level,
-there is no way (TIKO) in JS to recover & restart the 'thrown' computation.
-
-(from top level through the mutually-recursive calls to the voluntary 'yield')
-So: to return control from the voluntary yield, 
-up through the nested or mutually-recursive functions, 
-all the way to the system EventLoop, we do 3 things:
-1: Each function [caller & called] must be declared as function* (so it can yield)
-2: Each function [caller] must test whether its [called] descendant suspended, 
-   and if so, yield itself to propagate the 'yield' to the top level:
-   let result, genR = calledStarFunction(args);
-   while (result = genR.next(), !result.done) yield;
-   use (result.value)
-Note: #2 cannot be wrapped in a function... because that function would be subject to #1
-3: At the top-levl, use setTimeout(() => genR.next()) return to the JS EventLoop
-   and then restart the chain of suspended functions.
-
-Note: most documented usage of function* are to create a Generator, in a case where
-'yield' provides the interesting/useful value, and 'return' simply indicates its done.
-In this use-case its inverted: yield gives a signal, but no interesting value,
-and 'return' supplies the interesting computational value.
-
-Appeal to the JS Gods:
-Provide a function: runEventLoop()
-That transparently puts the current continuation (the full stack) on the event loop
-and return control directly to the top-level. 
-so all the other callers and the call stack 
-do not need to be aware of the suspend/resume beind done at the lower level
+https://stackoverflow.com/questions/71963376/how-to-enable-javascript-eventloop-when-running-nested-recursive-computation/71963377#71963377
  */ 
 /** <yield: void, return: TReturn, yield-in: unknown> */
 //                            Generator<yield, return, yield-in>
@@ -57,14 +16,9 @@ export type YieldR<TReturn> = Generator<void, TReturn, unknown>
  * @param done 
  */
 export function allowEventLoop<T>(genR: YieldR<T>, done?: (result: T) => void): void  {
-  console.log(stime('allowEventLoop', ` ENTER ->`), genR)
   let result = genR.next()
-  console.log(stime('allowEventLoop', ` result =`), result)
   if (result.done) done && done(result.value)
-  else {
-    console.log(stime('allowEventLoop', ' setTimeout ->'), genR)
-    setTimeout(() => allowEventLoop(genR, done))
-  }
+  else setTimeout(() => allowEventLoop(genR, done))
 }
 /** 
  * Return next result from genR. 
