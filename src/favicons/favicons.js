2fe40779e80ac7158021118d7336c62157d69673
jackpunt <jpeckj@gmail.com>
2024-08-31 10:38:21 -0700

    ng@17: rm custom-webpack & require-context; favicons.js ng@17: wicg-file-system-access@2020.9.8

--- src/favicons/favicons.js
+++ src/favicons/favicons.js
@@ -1,9 +1,11 @@
 // https://medium.com/tech-angels-publications/bundle-your-favicons-with-webpack-b69d834b2f53
+// npm i require-context --save
 
-const faviconsContext = require.context(
-    '!!file-loader?name=src/favicons/[name].[ext]!.',
-    true,
-    /\.(svg|png|ico|xml|json)$/
-  );
-  console.log(`favicons.js:`,faviconsContext.keys())
-  faviconsContext.keys().forEach(faviconsContext);
+// FAILS with Angular-17, but it wasn't being used before that either:
+// const faviconsContext = require.context(
+//     '!!file-loader?name=src/favicons/[name].[ext]!.',
+//     true,
+//     /\.(svg|png|ico|xml|json)$/
+//   );
+//   console.log(`favicons.js:`,faviconsContext.keys())
+//   faviconsContext.keys().forEach(faviconsContext);
